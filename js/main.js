9d6f3a0cdfa2119c9a473cff508b56c01c27cfaf
qseboy <qseboy1998@gmail.com>
2019-05-30 00:25:49 +0300

    Latest Works готовое

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $('button[filter="web"]').click(function() {
+        if ($(this).attr('val')=='off') {
+            $('button[filter]').attr('val','off');
+            $(this).attr('val' , 'on');
+            $('.filter-work > div').hide(300);
+            $('.filter-work > [filter="web"]').show(300);
+        }
+    });
+
+    $('button[filter="ui"]').click(function() {
+        if ($(this).attr('val')=='off') {
+            $('button[filter]').attr('val','off');
+            $(this).attr('val' , 'on');
+            $('.filter-work > div').hide(300);
+            $('.filter-work > [filter="ui"]').show(300);
+        }
+    });
+
+    $('button[filter="moc"]').click(function() {
+        if ($(this).attr('val')=='off') {
+            $('button[filter]').attr('val','off');
+            $(this).attr('val' , 'on');
+            $('.filter-work > div').hide(300);
+            $('.filter-work > [filter="moc"]').show(300);
+        }
+    });
+
+    $('button[filter="all"]').click(function() {
+        if ($(this).attr('val')=='off') {
+            $('button[filter]').attr('val','off');
+            $(this).attr('val' , 'on');
+            $('.filter-work > div').show(300);
+        }
+    });
+
+    $('button[filter="viev-all"]').click(function() {
+        if ($(this).attr('val')=='off') {
+            $('button[filter]').attr('val','off');
+            $(this).attr('val' , 'on');
+            $('.filter-work > div').show(300);
+        }
+    });
+});
