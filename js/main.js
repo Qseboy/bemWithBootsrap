adca373808dd8e285c4d73bc9f5f5ca59d88c7d6
qseboy <qseboy1998@gmail.com>
2019-05-31 11:14:27 +0300

    team готовое

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-$(document).ready(function() {
-    $('button[filter="web"]').click(function() {
-        if ($(this).attr('val')=='off') {
-            $('button[filter]').attr('val','off');
-            $(this).attr('val' , 'on');
+$(document).ready(function () {
+    $('button[filter="web"]').click(function () {
+        if ($(this).attr('val') == 'off') {
+            $('button[filter]').attr('val', 'off');
+            $(this).attr('val', 'on');
             $('.filter-work > div').hide(300);
             $('.filter-work > [filter="web"]').show(300);
         }
     });
 
-    $('button[filter="ui"]').click(function() {
-        if ($(this).attr('val')=='off') {
-            $('button[filter]').attr('val','off');
-            $(this).attr('val' , 'on');
+    $('button[filter="ui"]').click(function () {
+        if ($(this).attr('val') == 'off') {
+            $('button[filter]').attr('val', 'off');
+            $(this).attr('val', 'on');
             $('.filter-work > div').hide(300);
             $('.filter-work > [filter="ui"]').show(300);
         }
     });
 
-    $('button[filter="moc"]').click(function() {
-        if ($(this).attr('val')=='off') {
-            $('button[filter]').attr('val','off');
-            $(this).attr('val' , 'on');
+    $('button[filter="moc"]').click(function () {
+        if ($(this).attr('val') == 'off') {
+            $('button[filter]').attr('val', 'off');
+            $(this).attr('val', 'on');
             $('.filter-work > div').hide(300);
             $('.filter-work > [filter="moc"]').show(300);
         }
     });
 
-    $('button[filter="all"]').click(function() {
-        if ($(this).attr('val')=='off') {
-            $('button[filter]').attr('val','off');
-            $(this).attr('val' , 'on');
+    $('button[filter="all"]').click(function () {
+        if ($(this).attr('val') == 'off') {
+            $('button[filter]').attr('val', 'off');
+            $(this).attr('val', 'on');
             $('.filter-work > div').show(300);
         }
     });
 
-    $('button[filter="viev-all"]').click(function() {
-        if ($(this).attr('val')=='off') {
-            $('button[filter]').attr('val','off');
-            $(this).attr('val' , 'on');
+    $('button[filter="viev-all"]').click(function () {
+        if ($(this).attr('val') == 'off') {
+            $('button[filter]').attr('val', 'off');
+            $(this).attr('val', 'on');
             $('.filter-work > div').show(300);
         }
     });
