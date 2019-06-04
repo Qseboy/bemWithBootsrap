91ba163ab099e88773d6dfc0fecb48d3c99d2185
qseboy <qseboy1998@gmail.com>
2019-06-04 22:02:01 +0300

    Готовый адаптив 04.06

--- js/main.js
+++ js/main.js
@@ -41,4 +41,39 @@ $(document).ready(function () {
             $('.filter-work > div').show(300);
         }
     });
+    /*меню*/
+    $('ul.menu a[href^="#"').click(function(){
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+        return false; 
+    });
+    
+    /* Кнопка вверх */
+    $(window).scroll(function(){
+        if ($(this).scrollTop() != 0)
+            $('#toTop').fadeIn();
+        else
+            $('#toTop').fadeOut();
+    });
+    $("#toTop").click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    /*Выпадающее меню*/
+    $('.menu-icon').click(function(){
+        $('nav').slideToggle(500);
+        $('ul').css({
+            'display':'flex', 'flex-direction':'column'
+        })
+        if($('.menu-icon').html() == '<i class="fas fa-bars"></i>') { // проверка с содержимым хтмл
+            $(this).html('<i class="fas fa-times"></i>');
+        } else {
+            $(this).html('<i class="fas fa-bars"></i>');
+        } 
+    })
+
 });
